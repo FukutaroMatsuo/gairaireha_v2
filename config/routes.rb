da64793b0419fafa43fb6e26ba976132af0c42f8
FukutaroMatsuo <54502796+FukutaroMatsuo@users.noreply.github.com>
2021-01-13 23:24:20 +0900

    Rails+Nginxの本番環境の構築

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: "rehas#index"
+  # root to: "static_pages#index"
   resources :users, only: [:show]
   resources :patients
   resources :revenues
   resources :rehas, except: [:index]
   get "/day", to: "rehas#day", as: "day"
   get "list/:day", to: "rehas#list", as: "list"
-  # get "list/:day/edit", to: "rehas#edit_list", as: "edit_list"
 end
---
+++ docker/web/nginx/start.sh
@@ -0,0 +1,3 @@
+envsubst '$RAILS_ROOT' < /tmp/app.conf > /etc/nginx/conf.d/default.conf
+
+nginx -g "daemon off;"
